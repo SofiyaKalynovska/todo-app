a905b3cd0cadcfa1da9c72c1da93e86d72661572
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-11-17 20:00:00 +0200

    Add reducers for deleting, channging status, filter

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -15,15 +15,37 @@ const initialState = {
 
 export const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "tasks/addTask": {
+    case "tasks/addTask":
       return {
         ...state,
-        tasks: [
-          ...state.tasks,
-          action.payload,
-        ],
+        tasks: [...state.tasks, action.payload],
       };
-    }
+    case "tasks/deleteTask":
+      return {
+        ...state,
+        tasks: state.tasks.filter(task => task.id !== action.payload),
+      };
+    case "tasks/toggleCompleted":
+      return {
+        ...state,
+        tasks: state.tasks.map(task => {
+          if (task.id === action.payload) {
+            return {
+              ...task,
+              completed: !task.completed,
+            };
+          }
+          return task;
+        }),
+      };
+    case "filters/setStatusFilter":
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          status: action.payload,
+        },
+      };
     default:
       return state;
   }
